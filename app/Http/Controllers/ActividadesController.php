02e9a3488a874d693193cea9c1f015d6e3e34a40
Jorge Pineda <aplicacionesj@gmail.com>
2018-10-18 04:25:59 +0000

    Estilización tercer nivel

--- app/Http/Controllers/ActividadesController.php
+++ app/Http/Controllers/ActividadesController.php
@@ -22,9 +22,6 @@ class ActividadesController extends Controller
         }, 'multimediasActividades' => function($queryMultimediasActividades){
             $queryMultimediasActividades->orderBy('portada', 'desc')->select('actividades_id', 'ruta');
         }, 'sitiosConActividades' => function ($querySitiosConActividades){
-<<<<<<< HEAD
-            $querySitiosConActividades->join('atracciones', 'sitios.id', '=', 'atracciones.sitios_id')->select('sitios.id as id' ,'sitios.latitud as latitud', 'sitios.longitud as longitud');
-=======
             $querySitiosConActividades->with(['sitiosConIdiomas' => function($querySitiosConIdiomas){
                 $querySitiosConIdiomas->orderBy('idiomas_id')->select('idiomas_id', 'sitios_id', 'nombre', 'descripcion');
             }])->select('sitios.id', 'sitios.latitud', 'sitios.longitud');
@@ -36,7 +33,6 @@ class ActividadesController extends Controller
             $queryCategoriaTurismoConActividades->with(['categoriaTurismoConIdiomas' => function ($queryCategoriaTurismoConIdiomas){
                 $queryCategoriaTurismoConIdiomas->orderBy('idiomas_id')->select('categoria_turismo_id', 'idiomas_id', 'nombre');
             }])->select('categoria_turismo.id');
->>>>>>> 4f0853a5774bcc7a8fc97701a05e4f18927a22c2
         }])->where('id', $id)->select('id', 'valor_min', 'valor_max', 'calificacion_legusto', 'calificacion_llegar', 'calificacion_recomendar', 'calificacion_volveria')->first();
         
         //return ['actividad' => $actividad];
--- resources/views/actividades/Ver.blade.php
+++ resources/views/actividades/Ver.blade.php
@@ -10,28 +10,120 @@ function parse_yturl($url)
 ?>
 @extends('layout._publicLayout')
 
-@section('Title','Actividades')
+@section('Title',$actividad->actividadesConIdiomas[0]->nombre)
 
 @section('TitleSection','Actividades')
 
+@section ('estilos')
+    <link href="{{asset('/css/public/pages.css')}}" rel="stylesheet">
+    @if(count($actividad->multimediasActividades) == 0)
+        <style>
+            header {
+                position: relative;
+                background: black;
+                margin-bottom: 1rem;
+            }
+        </style>
+    @endif
+@endsection
+
+@section('meta_og')
+<meta property="og:title" content="Conoce {{$actividad->actividadesConIdiomas[0]->nombre}} en el departamento del Magdalena" />
+<meta property="og:image" content="{{asset('/res/img/brand/128.png')}}" />
+<meta property="og:description" content="{{$actividad->actividadesConIdiomas[0]->descripcion}}"/>
+@endsection
+
 @section('content')
+    
+    <div id="carousel-main-page" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        @for($i = 0; $i < count($actividad->multimediasActividades); $i++)
+            <li data-target="#carousel-main-page" data-slide-to="{{$i}}" {{  $i === 0 ? 'class=active' : '' }}></li>
+        @endfor
+      </ol>
+    
+      <!-- Wrapper for slides -->
+        
+      
+      <div class="carousel-inner">
+        @for($i = 0; $i < count($actividad->multimediasActividades); $i++)
+        <div class="item {{  $i === 0 ? 'active' : '' }}">
+          <img src="{{$actividad->multimediasActividades[$i]->ruta}}" alt="Imagen de presentación de {{$actividad->actividadesConIdiomas[0]->nombre}}">
+          
+        </div>
+        @endfor
+        
+      </div>
+      <div class="carousel-caption">
+          <h2>{{$actividad->actividadesConIdiomas[0]->nombre}}
+              <small class="btn-block">
+	              <span class="{{ ($actividad->calificacion_legusto > 0.0) ? (($actividad->calificacion_legusto <= 0.9) ? 'ionicons-inline ion-android-star-half' : 'ionicons-inline ion-android-star'}}" aria-hidden="true"></span>
+	              <span class="{{ ($actividad->calificacion_legusto > 1.0) ? (($actividad->calificacion_legusto <= 1.9) ? 'ionicons-inline ion-android-star-half' : 'ionicons-inline ion-android-star'}}" aria-hidden="true"></span>
+	              <span class="{{ ($actividad->calificacion_legusto > 2.0) ? (($actividad->calificacion_legusto <= 2.9) ? 'ionicons-inline ion-android-star-half' : 'ionicons-inline ion-android-star'}}" aria-hidden="true"></span>
+	              <span class="{{ ($actividad->calificacion_legusto > 3.0) ? (($actividad->calificacion_legusto <= 3.9) ? 'ionicons-inline ion-android-star-half' : 'ionicons-inline ion-android-star'}}" aria-hidden="true"></span>
+	              <span class="{{ ($actividad->calificacion_legusto > 4.0) ? (($actividad->calificacion_legusto <= 5.0) ? 'ionicons-inline ion-android-star-half' : 'ionicons-inline ion-android-star'}}" aria-hidden="true"></span>
+	              <span class="sr-only">Posee una calificación de {{$actividad->calificacion_legusto}}</span>
+	            
+	          </small>
+          </h2>
+          <div class="text-center">
+            @if(Auth::check())
+                <button class="btn btn-lg btn-circled btn-favorite">
+                  <span class="ion-android-favorite" aria-hidden="true"></span><span class="sr-only">Marcar como favorito</span>
+                </button>
+                
+            @else
+                <button type="button" class="btn btn-lg btn-circled" title="Marcar como favorito" data-toggle="modal" data-target="#modalIniciarSesion">
+                  <span class="ion-android-favorite-outline" aria-hidden="true"></span><span class="sr-only">Marcar como favorito</span>
+                </button>
+            @endif
+              
+          </div>
+      </div>
+      <!-- Controls -->
+      <!--<a class="left carousel-control" href="#carousel-main-page" role="button" data-slide="prev">-->
+      <!--  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
+      <!--  <span class="sr-only">Anterior</span>-->
+      <!--</a>-->
+      <!--<a class="right carousel-control" href="#carousel-main-page" role="button" data-slide="next">-->
+      <!--  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
+      <!--  <span class="sr-only">Siguiente</span>-->
+      <!--</a>-->
+    </div>
+    <div id="menu-page">
+        <div class="container">
+            <ul id="menu-page-list" class="justify-content-center">
+                <li>
+                    <a href="#informacionGeneral" class="toSection">
+						<i class="ionicons ion-information-circled" aria-hidden="true"></i>
+						<span class="hidden-xs">Información general</span>
+					</a>
+                </li>
+                <li>
+                    <a href="#caracteristicas" class="toSection">
+						<i class="ionicons ionicons ion-android-pin" aria-hidden="true"></i>
+						<span class="hidden-xs">Ubicación</span>
+					</a>
+                </li>
+                <li>
+                    <a href="#comentarios" class="toSection">
+						<i class="ionicons ion-chatbubbles" aria-hidden="true"></i>
+						<span class="hidden-xs">Comentarios</span>
+					</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+    
+
     <div class="row">
         <div class="col-sm-12 col-md-12 col-xs-12 text-center">
             <h1>Nombre: {{$actividad->actividadesConIdiomas[0]->nombre}}</h1>
         </div>
     </div>
-    {{-- La posición 0 es la portada --}}
-    <h3>Portada</h3>
-    <div class="row">
-        <img src="{{$actividad->multimediasActividades[0]->ruta}}"></img>
-    </div>
-    <h3>Imágenes</h3>
-    <div class="row">
-        {{-- La cuenta empieza desde 1 porque la primera posición es la portada --}}
-        @for($i = 1; $i < count($actividad->multimediasActividades); $i++)
-        <img src="{{$actividad->multimediasActividades[$i]->ruta}}"></img>
-        @endfor
-    </div>
+    
     <h3>Descripción</h3>
     <div class="row">
         <div class="col-md-3 col-sm-3 col-xs-12 col-md-offset-4 col-sm-offset-4">
