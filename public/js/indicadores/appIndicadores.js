f941c75bd8772450323b5c92e8e7defb462081c4
Brayan Rene Carbono Carbono <yambra12@gmail.com>
2018-11-13 22:18:39 +0000

    Indicadores

--- public/js/indicadores/appIndicadores.js
+++ public/js/indicadores/appIndicadores.js
@@ -25,49 +25,10 @@
            
     var app = angular.module("appIndicadores", [  "chart.js", "indicadoresServices" ] );
    
-    app.controller("receptorCtrl", ["$scope","indicadoresServi", function($scope,indicadoresServi){
+    app.controller("IndicadoresCtrl", ["$scope","indicadoresServi", function($scope,indicadoresServi){
         
-        $scope.options = {
-                            backgroundColor:'rgb(10,10,10)',
-                            legend: { display: true, position: 'bottom', labels: { fontColor: 'black' }, },
-                            scales: { 
-                                      xAxes: [{ 
-                                                display: true, 
-                                                offset:true,
-                                                scaleLabel: { display:true, fontSize:14, fontColor: "black", labelString:"" },
-                                                ticks: {
-                                                        callback: function(tickValue, index, ticks) {
-                                                            return tickValue.length<=20 ? tickValue : tickValue.substring(0, 17) +"..." ;
-                                                        }
-                                                }
-                                      }], 
-                                      yAxes: [{ display: true, offset:true, scaleLabel: { display:true, fontSize:14, fontColor: "black", labelString:"" } }] 
-                            },
-                            title: {  display: true, text: '', fontSize:16  },
-                            tooltips: {
-                                enabled: true,
-                                mode: 'label',
-                                callbacks: {
-                                    title: function(tooltipItems, data) {
-                                        var idx = tooltipItems[0].index;
-                                        return data.labels[idx];
-                                    }
-                                }
-                            },
-                            plugins: {
-                                datalabels: {
-                                    display: true,
-                                    color: 'black',
-                                    align: 'bottom',
-                                    anchor: 'end',
-                                    formatter: function(value,context) {
-                                        return value +' '+ ($scope.graficaSelect.codigo !='pie' ? $scope.formato : '%' );
-                                    }
-                                }
-                            }
-                            
-                            
-                        };
+        $scope.options = optionsGraficas;
+        $scope.colores = coloresGraficas;
         
         
         $scope.filtrarDatos = function(){
@@ -100,7 +61,10 @@
                     
                     $scope.yearSelect = data.periodos[0];
                     $scope.filtro.year = $scope.yearSelect.year;
+                    if($scope.yearSelect.mes){ $scope.filtro.mes = $scope.yearSelect.mes; }
+                    else if($scope.yearSelect.meses){ $scope.filtro.mes = $scope.yearSelect.meses[0].id; }
                     $scope.filtro.mes = $scope.yearSelect.mes;
+                    $scope.filtro.temporada =  $scope.yearSelect.temporadas ? $scope.yearSelect.temporadas[0].id : null;
                     
                     $scope.inicializarDataGrafica(data.data);
                     
@@ -112,16 +76,34 @@
         
         $scope.changeTipoGrafica = function(item){
             $scope.graficaSelect = item;
-            $scope.options.scales.xAxes[0].display = !(item.codigo=="pie");
-            $scope.options.scales.yAxes[0].display = !(item.codigo=="pie");
-            $scope.options.legend.display = ($scope.series || item.codigo=="pie") ? true : false;
             
-            $scope.override = item.codigo=="line" ? { borderWidth: 3, fill:false } : null;
-            if( item.codigo=="line" && $scope.series ){
-                $scope.override = [];
-                for(var i=0;i<$scope.series.length>0;i++){
-                    $scope.override.push({ borderWidth: 3, fill:false });
+            var validar_tipo_grafica = (item.codigo=="pie" || item.codigo=="doughnut" || item.codigo=="polarArea" || item.codigo=="radar");
+            
+            $scope.options.scales.xAxes[0].display = !validar_tipo_grafica;
+            $scope.options.scales.yAxes[0].display = !validar_tipo_grafica;
+            $scope.options.legend.display = validar_tipo_grafica;
+            $scope.override = [];
+            
+            if( validar_tipo_grafica && $scope.data.length>0 ){
+                $scope.override.push({ backgroundColor: [], borderColor: "white" });
+                for(var i=0;i<$scope.data[0].length>0;i++){
+                   $scope.override[0].backgroundColor.push( $scope.getColor() );
                 }
+            }
+            else if(item.id==4){ // graficas de torta
+                $scope.override.push({ borderWidth: ( item.codigo!="line" ? 2 : 3) , pointBorderColor: $scope.colores[i]  });
+            }
+            else{
+                $scope.override.push({ borderWidth: ( item.codigo!="line" ? 2 : 3) , fill:false, pointBorderColor: $scope.colores[i]  });
+            }
+            
+            if(item.codigo=="horizontalBar"){
+                $scope.options.scales.xAxes[0].scaleLabel.labelString = $scope.label_y;
+                $scope.options.scales.yAxes[0].scaleLabel.labelString = $scope.label_x;
+            }
+            else{
+                $scope.options.scales.xAxes[0].scaleLabel.labelString = $scope.label_x;
+                $scope.options.scales.yAxes[0].scaleLabel.labelString = $scope.label_y;
             }
             
         }   
@@ -140,15 +122,21 @@
             }
             $scope.options.title.text = $scope.tituloIndicadorGrafica;
             
-            $scope.colores = [ ];
-            for(var i=1; i<$scope.data.length; i++){ $scope.colores.push($scope.getColor()); }
-            
             for(var i=0; i<$scope.indicador.graficas.length; i++){
                 if($scope.indicador.graficas[i].pivot.es_principal){
                     $scope.changeTipoGrafica($scope.indicador.graficas[i]);
                     break;
                 }
             }
+            
+        }
+        
+        
+        $scope.getYear = function(id){
+            for(var i=0; i<$scope.periodos.length; i++){
+                if($scope.periodos[i].id==id){ return $scope.periodos[i].anio; }
+            }
+            return null;
         }
         
         $scope.getColor = function(){
@@ -157,15 +145,46 @@
             var r2 = Math.floor(Math.random()*256) ;
             var r3 = Math.floor(Math.random()*256) ;
         
-            return  {
-                      backgroundColor: "rgba("+r1+","+r2+","+r3+", 0.5)",
-                      pointBackgroundColor: "rgba("+r1+","+r2+","+r3+", 0.5)",
-                      pointHoverBackgroundColor: "rgb("+r1+","+r2+","+r3+")",
-                      borderColor: "rgb("+r1+","+r2+","+r3+")",
-                      pointBorderColor: '#fff',
-                      pointHoverBorderColor: "rgb("+r1+","+r2+","+r3+")"
-                    };
+            return  "rgba("+r1+","+r2+","+r3+", 0.5)";
         }
+        
+        Chart.plugins.register({
+			afterDatasetsDraw: function(chart) {
+				var ctx = chart.ctx;
+
+				chart.data.datasets.forEach(function(dataset, i) {
+					var meta = chart.getDatasetMeta(i);
+					if (!meta.hidden) {
+						meta.data.forEach(function(element, index) {
+							// Draw the text in black, with the specified font
+							ctx.fillStyle = 'rgb(0, 0, 0)';
+
+							var fontSize = 12;
+							var fontStyle = 'normal';
+							var fontFamily = 'Helvetica Neue';
+							ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+
+							// Just naively convert to string for now
+							var dataString = dataset.data[index].toString();
+
+							// Make sure alignment settings are correct
+							ctx.textAlign = 'center';
+							ctx.textBaseline = 'middle';
+                            
+                            
+                            dataString = element.hidden ? "" : dataString +' '+ ( $scope.graficaSelect.codigo !='pie' ? ($scope.formato?$scope.formato:'') : '%' );
+                            
+                            
+                            
+							var padding = 5;
+							var position = element.tooltipPosition();
+							var y = position.y  +  ($scope.graficaSelect.codigo !='pie' ?  25 : 0) - (fontSize / 2) - padding
+							ctx.fillText(dataString, position.x , y );
+						});
+					}
+				});
+			}
+		});
         
     }]);
     
