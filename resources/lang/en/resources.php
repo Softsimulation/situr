6c2a73638a29ce49fd2d0ce5bb25c44bf5265991
Jorge Pineda <aplicacionesj@gmail.com>
2018-11-13 22:15:24 +0000

    Creación de resources

--- resources/lang/en/resources.php
+++ resources/lang/en/resources.php
@@ -3,6 +3,64 @@
 return [
     
     
+    "menu"          => [
+        "inicio" => "Home",
+        "acercaDe" => "About Alántico",
+        "requisitos"  => "Travel requirements",
+        "quienesSomos"   => "About us?",
+        "equipoSITUR" => "SITUR Crew",
+        "viveElAtlantico" => "Vive el Atlántico",
+        "mapa" => "Atlántico Map",
+        "estadisticas" => "Stats",
+        "publicaciones" => "Publications",
+        "contactanos" => "Contact us"
+    ],
+    "entidad" =>[
+        "destinos" => "Destinos",
+        "eventos" => "Eventos",
+        "actividades" => "Actividades",
+        "atracciones" => "Atracciones",
+        "rutasTuristicas" => "Rutas turísticas",
+        "guias" => "Guías",
+        "alojamientos" => "Alojamientos",
+        "restaurantes" => "Restaurantes"
+    ],
+    "estadisticas" =>[
+        "receptor" => "Turísmo receptor",
+        "interno" => "Turísmo interno y emisor",
+        "empleo" => "Empleo",
+        "oferta" => "Oferta",
+        "sostenibilidad" => "Sostenibilidad",
+        "sostenible" => "Turísmo sostenible",
+        "secundarias" => "Indicadores secundarios"
+    ],
+    "publicaciones" =>[
+        "informes" => "Informes",
+        "noticias" => "Noticias",
+        "bibliotecaDigital" => "Biblioteca digital",
+        "bolsaDeEmpleo" => "Bolsa de empleo"
+    ],
+    "common" => [
+        "siguiente" => "Siguiente",
+        "anterior" => "Anterior",
+        "verMas" => "Ver más",
+        "verTodo" => "Ver todo",
+        "cargando" => "Cargando. Por favor espere...",
+        "menuDeNavegacion" => "Menú de navegación",
+        "irAlContenido" => "Ir al contenido",
+        "busqueda" => "Búsqueda",
+        "buscar" => "Buscar",
+        "seleccionarIdioma" => "Seleccionar un idioma",
+        "iniciarSesion" => "Iniciar sesión",
+        "cerrarSesion" => "Cerrar sesión",
+        "logoDe" => "Logo de :logo",
+        "contacto" => "Contacto",
+        "enlacesDeInteres" => "Enlaces de intéres",
+        "telefono" => "Teléfono"
+    ]
+    
+    
+    
     
     
     
--- resources/lang/es/resources.php
+++ resources/lang/es/resources.php
@@ -20,8 +20,63 @@ return [
     'EncuestaFuenteInfoP6Input1'=>'Escriba su cuenta de Facebook',
     'EncuestaFuenteInfoP7Input1'=>'Escriba su cuenta de Twitter',
     'EncuestaBtnAnterior'=>'Anterior',
-    'EncuestaBtnSiguiente'=>'Siguiente'
+    'EncuestaBtnSiguiente'=>'Siguiente',
     
+    "menu"          => [
+        "inicio" => "Inicio",
+        "acercaDe" => "Acerca del departamento",
+        "requisitos"  => "Requisitos de viaje",
+        "quienesSomos"   => "¿Quiénes somos?",
+        "equipoSITUR" => "Equipo SITUR",
+        "viveElAtlantico" => "Vive el Atlántico",
+        "mapa" => "Mapa del departamento",
+        "estadisticas" => "Estadísticas",
+        "publicaciones" => "Publicaciones",
+        "contactanos" => "Contáctanos"
+    ],
+    "entidad" =>[
+        "destinos" => "Destinos",
+        "eventos" => "Eventos",
+        "actividades" => "Actividades",
+        "atracciones" => "Atracciones",
+        "rutasTuristicas" => "Rutas turísticas",
+        "guias" => "Guías",
+        "alojamientos" => "Alojamientos",
+        "restaurantes" => "Restaurantes"
+    ],
+    "estadisticas" =>[
+        "receptor" => "Turísmo receptor",
+        "interno" => "Turísmo interno y emisor",
+        "empleo" => "Empleo",
+        "oferta" => "Oferta",
+        "sostenibilidad" => "Sostenibilidad",
+        "sostenible" => "Turísmo sostenible",
+        "secundarias" => "Indicadores secundarios"
+    ],
+    "publicaciones" =>[
+        "informes" => "Informes",
+        "noticias" => "Noticias",
+        "bibliotecaDigital" => "Biblioteca digital",
+        "bolsaDeEmpleo" => "Bolsa de empleo"
+    ],
+    "common" => [
+        "siguiente" => "Siguiente",
+        "anterior" => "Anterior",
+        "verMas" => "Ver más",
+        "verTodo" => "Ver todo",
+        "cargando" => "Cargando. Por favor espere...",
+        "menuDeNavegacion" => "Menú de navegación",
+        "irAlContenido" => "Ir al contenido",
+        "busqueda" => "Búsqueda",
+        "buscar" => "Buscar",
+        "seleccionarIdioma" => "Seleccionar un idioma",
+        "iniciarSesion" => "Iniciar sesión",
+        "cerrarSesion" => "Cerrar sesión",
+        "logoDe" => "Logo de :logo",
+        "contacto" => "Contacto",
+        "enlacesDeInteres" => "Enlaces de intéres",
+        "telefono" => "Teléfono"
+    ]
     
     
     
