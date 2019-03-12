1c3b5e55c9f2c59e401b53f07fd9715608a983ed
Brayan Rene Carbono Carbono <yambra12@gmail.com>
2019-03-12 14:23:18 +0000

    Cambios indicadores

--- resources/views/indicadores/index.blade.php
+++ resources/views/indicadores/index.blade.php
@@ -330,10 +330,10 @@
 
 
 @section('javascript')
+
     <script src="{{asset('/js/plugins/jspdf.min.js')}}"></script>
     <script src="{{asset('/js/plugins/Chart.min.js')}}"></script>
     <script src="{{asset('/js/plugins/angular-chart.min.js')}}"></script>
-    <script src="{{asset('/js/plugins/chartsjs-plugin-data-labels.js')}}"></script>
     <script src="{{asset('/js/plugins/angular-filter.js')}}"></script>
     <script src="{{asset('/js/indicadores/appIndicadores.js')}}"></script>
     <script src="{{asset('/js/indicadores/servicios.js')}}"></script> 
