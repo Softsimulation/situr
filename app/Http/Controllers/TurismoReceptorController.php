3038c022bcc20764394115ffd44f2a208e54b884
Camilo Torres <ctorresunimag95@gmail.com>
2019-05-07 20:14:53 +0000

    Listado rango receptor

--- app/Http/Controllers/TurismoReceptorController.php
+++ app/Http/Controllers/TurismoReceptorController.php
@@ -742,6 +742,12 @@ class TurismoReceptorController extends Controller
         return $encuestas;
     }
     
+    public function getEncuestasrango($fecha_inicial, $fecha_final){
+        $encuestas = \DB::select('select * from encuestas_turismo_receptor(?,?)',array($fecha_inicial,$fecha_final));
+        
+        return ["encuestas" => $encuestas];
+    }
+    
     public function getListadoencuestas(){
         return view('turismoReceptor.Encuestas');
     }
